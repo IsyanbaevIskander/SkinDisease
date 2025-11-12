30eddd5b65f2f5d197b25e13d733bbe1faae98b4
ivan <vanya190104@gmail.com>
2025-11-12 16:19:27 +0500

    исправил urls

--- core/urls.py
+++ core/urls.py
@@ -10,8 +10,8 @@ router = DefaultRouter()
 
 router.register('users', views.UserView, basename='users')
 router.register('skin_conditions', views.SkinConditionView, basename='skin_conditions')
-router.register('diagnosis_request', views.DiagnosisRequestView, basename='diagnosis_request')
-router.register('diagnosis_result', views.DiagnosisResultView, basename='diagnosis_result')
-router.register('medical_verification', views.MedicalVerificationView, basename='medical_verification')
+router.register('diagnosis_requests', views.DiagnosisRequestView, basename='diagnosis_requests')
+router.register('diagnosis_results', views.DiagnosisResultView, basename='diagnosis_results')
+router.register('medical_verifications', views.MedicalVerificationView, basename='medical_verifications')
 
 urlpatterns += router.urls
